64a226f4bb4c6c6a453be2b987231e15d3e8ac83
KennedyJanto2 <Kennedyjanto3@gmail.com>
2023-10-23 01:42:21 -0700

    edit file title

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
+# AUTHOR: kennedy janto
+# FILENAME: db_connection
 # SPECIFICATION: description of the program
 # FOR: CS 4250- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 4 hours
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with
